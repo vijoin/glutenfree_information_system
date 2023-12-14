9c2c279d57261ccbb19cef6643953f635c43e124
Victor Inojosa <vijoin@gmail.com>
2023-12-14 19:05:18 -0300

    Add test case for product creation

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Product, ProductBrand, ProductCategory
 
-# Create your tests here.
+
+class ProductTest(TestCase):
+    def setUp(self):
+        brand = ProductBrand.objects.create(name="Brand1", description="Brand1")
+        category = ProductCategory.objects.create(name="Category1", description="Category1")
+        Product.objects.create(name="Product1", brand=brand, category=category)
+
+    def test_product_is_created(self):
+        product = Product.objects.get(name="Product1")
+        self.assertEqual(product.name, "Product1")
+        self.assertEqual(product.brand.name, "Brand1")
+        self.assertEqual(product.category.name, "Category1")
